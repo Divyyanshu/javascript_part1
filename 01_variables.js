4d8cb5ab088d43a68f762c9eed81c49e1cdddf32
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2023-09-18 11:08:38 +0530

    bvariable/datatypes

---
+++ 01_variables.js
@@ -0,0 +1,18 @@
+// const VS let VS var
+// implement for real time project 
+
+const accountId = 4561;
+let accountName = "divyasnhu";
+var accountEmail = "dtailor1881@gmail.com";
+let accountCity = "jaipur";
+let accountStatement;
+
+//  speciall..! var ko use na kre kyu ki isme  vlock scope or function scope ki dikht hai 
+
+console.table([
+  accountId,
+  accountEmail,
+  accountStatement,
+  accountName,
+  accountCity,
+]);
---
+++ 02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict";
+// Premetive type datatype;
+// USING mdm document
+
+let Name = "divyanshu"
+let age = 21
+let gender = "male"
+let itsLogined = true
+let temp = null
+let jam;
+// null => standalone value
+// undefined => empty value ; not give vlaue 
+//symbole => use in react for componet for using unique 
+
+console.table([Name , age,gender,temp,itsLogined,jam])
+console.log(typeof jam)
