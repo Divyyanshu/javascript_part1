00ac501e0cd8374510172f7a2db42e37bad35c91
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2023-09-19 14:00:37 +0530

    commit datetimes

---
+++ 05_strings.js
@@ -0,0 +1,45 @@
+// let name = "divyashu"
+// let age  = 25;
+// console.log(`my name is ${name} and my age is ${age}`)
+
+// string interplation `` and use ${}
+
+// .2 use declartion use new String
+
+// const gameName = new String("divyanshutailor")
+// console.log(gameName)
+// console.log(gameName[11])
+// console.log(gameName.length)
+// console.log(gameName.__proto__)
+// console.log(gameName.toUpperCase())
+// // for find postion for char give index no
+// console.log(gameName.charAt(8))
+// //  for find postion no of char give char  letter
+// console.log(gameName.indexOf("u"))
+
+// const newString = gameName.substring(0,10)
+// console.log(newString)
+
+// use splice for reverse string
+// const anotherstr = gameName.slice(5,0)
+// console.log(anotherstrcls)
+
+//trim the
+// const newString1 = "     Dhwani  "
+// console.log(newString1)
+// console.log(newString1.trim())
+
+// //replace
+// let url = "htttps:divyanshutailor/divyanshu20%tailor.com"
+// console.log(url.replace('20%' , '-'))
+// // for checking it is persent or not !
+// console.log(url.includes("divyanshu"))
+
+// const str = new String("divyanshutailor");
+// console.log(str.slice(5));
+// console.log(str.slice(4, 10));
+// console.log(str.slice(-4));
+// console.log(str.slice(-9, 1));
+
+// const gameName = new String("divyanshu-tailor")
+// console.log(gameName.split('-'));
