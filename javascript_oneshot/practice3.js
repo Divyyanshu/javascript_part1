c422c32fac9e1e04f0da53589ae4dd257fef2c6d
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2023-10-08 00:18:57 +0530

    calculator project 1

--- javascript_oneshot/practice3.js
+++ javascript_oneshot/practice3.js
@@ -1,1 +1,7 @@
 // alert('welcome')
+
+const myName = 'divyanshu tailor i am a web developer'
+document.querySelector("#name-display").innerText = myName;
+ 
+let cal = '0+120+60'
+console.log(eval(cal));
