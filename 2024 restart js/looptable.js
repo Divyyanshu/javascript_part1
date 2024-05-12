8ae1e05e3e92b9c355f7970322748097036f33cd
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2024-05-12 11:35:02 +0530

    2024 loop basic code

---
+++ 2024 restart js/looptable.js
@@ -0,0 +1,25 @@
+//table print krvalo
+// using backtick `` statement
+
+
+// for(let i= 0 ; i <= 50 ; i++){
+//     console.log(`65 x ${i} = ${65*i}`);
+// }
+
+// // while loop
+// let i = 1;
+// while(i <= 10){
+//     console.log(`77 x ${i} = ${77*i}`);
+//     i++;
+// }
+
+// do while
+function loopertable(){
+    var j = 1;
+do{
+    console.log(`25 x ${j} = ${25*j}`);
+    j++;
+}while(j<=10);
+}
+
+loopertable();
