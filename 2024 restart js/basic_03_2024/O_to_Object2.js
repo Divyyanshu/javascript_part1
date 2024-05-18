b3c74b3e1e614e6ec3d40148a97b5847e2d70753
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2024-05-18 22:20:43 +0530

    code

--- 2024 restart js/basic_03_2024/O_to_Object2.js
+++ 2024 restart js/basic_03_2024/O_to_Object2.js
@@ -1,16 +1,39 @@
-const obj1 = {
-    1 : 'a',
-    2 : "b"
-}
-const obj2 = {
-    3 : 'a',
-    4 : "b"
-}
-const obj3 = {
-    5 : 'a',
-    6 : "b"
+// const obj1 = {
+//     1 : 'a',
+//     2 : "b"
+// }
+// const obj2 = {
+//     3 : 'a',
+//     4 : "b"
+// }
+// const obj3 = {
+//     5 : 'a',
+//     6 : "b"
+// }
+
+// const obj4 = Object.assign({},obj1,obj2,obj3);
+// console.log(obj4)
+// console.log(obj4 === obj1);
+
+
+let mysymbol = Symbol ("bar");
+
+const beerBar = {
+    beer : "budwiser",
+    wisky : "signature",
+    monk : "ols monk",
+    hooka : {
+        preminum : {
+            floavored : ['red','green apple'],
+            tambakoo : ['advance','goldflake']
+        }
+    },
+    location : ()=>{
+        console.log("udaipur")
+   },
+   cigreet : "limon",
+   [mysymbol] : "happy-ending",
+   alcholicOrNot : true
 }
 
-const obj4 = Object.assign({},obj1,obj2,obj3);
-console.log(obj4)
-console.log(obj4 === obj1);
+console.log(beerBar);
