3add886e93275dbc9dd2232d6a302c2d7ad3c007
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2024-05-20 21:32:58 +0530

    array

--- 2024 restart js/Arry.js
+++ 2024 restart js/Arry.js
@@ -1,18 +1,26 @@
-// let name = ['divyanshu','btech','computer science', 80 , true ,'placemnet'];
-// name.push('devlopment');
-// name.unshift('mlvtextile');
-// name.splice(4,2,"total8semester")
-// console.log(name.index)
-// console.log(name);
-// console.table(name);
+// // let name = ['divyanshu','btech','computer science', 80 , true ,'placemnet'];
+// // name.push('devlopment');
+// // name.unshift('mlvtextile');
+// // name.splice(4,2,"total8semester")
+// // console.log(name.index)
+// // console.log(name);
+// // console.table(name);
 
-let degree = ['first degree' , 'third degree' , 'fouth degree' , 'fifth degree' , 'sixth degree'];
+// let degree = ['first degree' , 'third degree' , 'fouth degree' , 'fifth degree' , 'sixth degree'];
 
-degree.
+// degree.
 
-// degree.splice(1,0,'secound degree');
+// // degree.splice(1,0,'secound degree');
 
-// degree.push('seven degree');
+// // degree.push('seven degree');
 
-// degree.unshift('zero degree');
-console.table(degree);
+// // degree.unshift('zero degree');
+// console.table(degree);
+
+
+
+let homeName = ["harsh", "divyanshu" ,"sahaj" , "ankit"];
+
+homeName.splice(2,0,"mayank")
+
+console.log(homeName)
--- 2024 restart js/basic_03_2024/object_01.js
+++ 2024 restart js/basic_03_2024/object_01.js
@@ -1,27 +1,36 @@
-let mySym = Symbol ("drtailors");
+// let mySym = Symbol ("drtailors");
 
-let jurObject = {
-    name : "divyanshu tailor",
-    age : 22,
-    married : false,
-    [mySym] : "jai shree ram",
-    address : "bhilwara rajasthan",
-    skills : ["web development" , "app development" ,"AI/ML"]
+// let jurObject = {
+//     name : "divyanshu tailor",
+//     age : 22,
+//     married : false,
+//     [mySym] : "jai shree ram",
+//     address : "bhilwara rajasthan",
+//     skills : ["web development" , "app development" ,"AI/ML"]
+// }
+
+
+// console.log(jurObject.name)
+// console.log(jurObject.age)
+// console.log(jurObject.married)
+// console.log(jurObject.address)
+// console.log(jurObject.skills)
+// // Symbol datatype
+// console.log(jurObject[mySym]);
+
+// console.log(typeof jurObject.name)
+// console.log(typeof jurObject.age)
+// console.log(typeof jurObject.married)
+// console.log(typeof jurObject.address)
+// console.log(typeof jurObject.skills)
+
+// console.log(jurObject);
+
+
+const caFoundation = {
+    account : 56,
+    bussiness : "cainstituated",
+    king : "kota"
 }
 
-
-console.log(jurObject.name)
-console.log(jurObject.age)
-console.log(jurObject.married)
-console.log(jurObject.address)
-console.log(jurObject.skills)
-// Symbol datatype
-console.log(jurObject[mySym]);
-
-console.log(typeof jurObject.name)
-console.log(typeof jurObject.age)
-console.log(typeof jurObject.married)
-console.log(typeof jurObject.address)
-console.log(typeof jurObject.skills)
-
-console.log(jurObject);
+console.log(caFoundation)
