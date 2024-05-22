562a89c996a24ba857a3e3f35044530b4017d8ec
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2024-05-22 12:37:33 +0530

    arrow fun

---
+++ 2024 restart js/basic_03_2024/arrow.js
@@ -0,0 +1,40 @@
+// function chai(){
+//     let username = "divyanshu"
+//     console.log(this)
+// }
+// chai();
+
+// arrow function
+
+// const chai =  () => {
+//     let username = "divyanshu"
+//     console.log(this)
+// }
+// chai();
+
+
+
+// const course = {
+//     courseName: "code with tailor",
+//     instructorName: "divyanshu tailor",
+//     welocmescreen: function() {
+//         console.log(`${this.courseName} , welcome to website `)
+//     }
+// }
+
+// course.welocmescreen();
+// course.courseName = "js with tailor"
+// course.welocmescreen();
+
+
+// const plus = (num1,num2) =>{
+//    return num1 + num2
+// }
+
+// console.log(plus(10,5));
+
+
+// const plus = (num1,num2) =>  num1 + num2
+ 
+// console.log(plus(14,5));
+
