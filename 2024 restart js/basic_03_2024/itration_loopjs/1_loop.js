13d89b822e7a1f59fab94c183db28a2f4df06f61
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2024-05-23 10:33:13 +0530

    add loop tables nested loops

---
+++ 2024 restart js/basic_03_2024/itration_loopjs/1_loop.js
@@ -0,0 +1,21 @@
+for(let i = 1; i < 10 ; i++){
+  console.log(`outer loop i`)
+  for(let j = 1; j < 10 ; j++){
+    // console.log(`inner loop ${j} and outer loop ${i}`);
+    // console.log(i + "*" + j + '='  + i*j)
+    for(let k = 1; k < 10 ; k++){
+        console.log(i + "*" + j + "*" + k +  '='  + i*j*k)
+      }
+  }
+  
+}
+
+
+for(let i = 1; i < 10 ; i++){
+    console.log(`outer loop i`)
+    for(let j = 1; j < 10 ; j++){
+      console.log(`inner loop ${j} and outer loop ${i}`);
+      console.log(i + "*" + j + '='  + i*j)
+    }
+    
+  }
