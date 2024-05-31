b78eb6799ebbf6cde2d30dd44f919740870efd1a
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2024-05-31 18:59:08 +0530

    comment one code

--- 2024 restart js/howjscodexecuted.js
+++ 2024 restart js/howjscodexecuted.js
@@ -1,10 +1,10 @@
-let val1 = 10;
-let val2 = 54;
+// let val1 = 10;
+// let val2 = 54;
 
-function addnum(num1,num2){
-    let total = num1 + num2;
-    return total
-}
+// function addnum(num1,num2){
+//     let total = num1 + num2;
+//     return total
+// }
 
-let result1 = addnum(val1,val2);
-let result2 = addnum(42,65);
+// let result1 = addnum(val1,val2);
+// let result2 = addnum(42,65);
